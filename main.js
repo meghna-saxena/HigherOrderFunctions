5abdbc91c66d4ec7bc85346712f14964b8541ffc
Meghna Srivastava <meghna.s1906@gmail.com>
2019-08-07 14:47:37 +0200

    initial data

---
+++ main.js
@@ -0,0 +1,20 @@
+const companies = [
+  { name: "Company One", category: "Finance", start: 1981, end: 1995 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1991, end: 2007 },
+  { name: "Company Four", category: "Finance", start: 1996, end: 2013 },
+  { name: "Company Five", category: "Retail", start: 1994, end: 2010 },
+  { name: "Company Six", category: "Retail", start: 1993, end: 2003 },
+  { name: "Company Seven", category: "Auto", start: 1999, end: 2006 },
+  { name: "Company Eight", category: "Finance", start: 1989, end: 1996 },
+  { name: "Company Nine", category: "Retail", start: 2011, end: 2016 },
+  { name: "Company Ten", category: "Auto", start: 1986, end: 1989 }
+];
+
+const ages = [33, 21, 28, 27, 42, 30, 26, 35, 22, 18, 12, 23];
+
+// forEach
+// filter
+// map
+// sort
+// reduce
